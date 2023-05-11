9b8b0d05b0b69098f7ae0a3dfa03676cfcbabe3b
Viktor <vvs-PathOfExile@yandex.ru>
2023-05-11 23:06:53 +0300

    included copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require("path");
  
 module.exports = {
@@ -36,6 +37,11 @@ module.exports = {
             template: "./src/index.html",
             filename: "index.html",
         }),
+        new CopyWebpackPlugin({
+            patterns: [
+              { from: 'src/assets', to: '' },
+            ],
+          }),
     ],
     devServer: {
         static: {
